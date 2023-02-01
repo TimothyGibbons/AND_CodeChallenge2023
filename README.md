3212c1adbb2c9f779da78e7f009797c2ef809838
Tim Gibbons <timothy.gibbons@and.digital>
2023-02-01 23:40:28 +0000

    solution

--- README.md
+++ README.md
@@ -2,4 +2,19 @@
 
 ## All Challenges are saved as individual branches. Please see branch for challenge code.
 
-- Challenge 1: "ControlC, Control V"
+## Challenge 1 - "Control C, Control V" :clipboard:
+Welcome back! Here's a fun little string manipulation challenge to kick things off. The challenge is to analyse these strings for any instances of [CTRL+C] and [CTRL+V]. When [CTRL+C] is encountered, the contents of the string before it should be 'copied' to a clipboard. Upon any instance of [CTRL+V] in the string, this clipboard should be pasted in its place. If [CTRL+V] is encountered before any corresponding [CTRL+C] then it should simply paste nothing.
+### Rewards:
+- :five:  Points are awarded for a working algorithm as described above
+- :three:  Further points are awarded for supporting [CTRL+X], which should remove the preceding text before copying it to the clipboard
+- :two:  Further points are awarded for validating your solution with a collection of unit tests
+### Example:
+For this example input:
+challenge("the first[CTRL+C] Coding Challenge was [CTRL+V] string manipulation task")
+Your solution might output:
+"the first Coding Challenge was the first string manipulation task"
+### Submission:
+When you have a solution, submit a link to your source code repo using the /submit command here on Slack. Please include any code and data required in order to test your solution easily. Submission closes 1st February, 17:00
+Good luck good luck good luck good luck (edited) 
+
+___
--- src/main/java/and/codechallenge/challenge1/Main.java
+++ src/main/java/and/codechallenge/challenge1/Main.java
@@ -1,29 +1,77 @@
 package and.codechallenge.challenge1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Main {
-	
-	String input = "[\n"
-			+ "  \"the big red[CTRL+C] fox jumps over [CTRL+V] lazy dog.\",\n"
-			+ "  \"[CTRL+V]the tall oak tree towers over the lush green meadow.\",\n"
-			+ "  \"the sun shines down[CTRL+C] on [CTRL+V][CTRL+C] the busy [CTRL+V].\",\n"
-			+ "  \"[CTRL+V]the tall oak tree towers over the lush green meadow.\",\n"
-			+ "  \"a majestic lion[CTRL+C] searches for [CTRL+V] in the tall grass.\",\n"
-			+ "  \"the shimmering star[CTRL+X]Twinkling in the dark, [CTRL+V] shines bright.\",\n"
-			+ "  \"[CTRL+X]a fluffy white cloud drifts [CTRL+V][CTRL+C] across the sky, [CTRL+V]\",\n"
-			+ "]";
 	
 	private static final String CONT_C = "[CTRL+C]";
 	private static final String CONT_V = "[CTRL+V]";
 	private static final String CONT_X = "[CTRL+X]";
 	
-	public Main() {
-		String output = input;
-		while (output.contains(CONT_C)) {
+	public Main() throws FileNotFoundException {
+		String output = readFile("src/main/java/and/codechallenge/challenge1/input.txt");
 		
+		String clipboard = "";
+		
+		
+		while (output.contains(CONT_C) || output.contains(CONT_X) || output.contains(CONT_V)) {
+			
+			int c = output.indexOf(CONT_C);
+			int x = output.indexOf(CONT_X);
+			int v = output.indexOf(CONT_V);
+			
+			if (c == -1) c = Integer.MAX_VALUE;
+			if (x == -1) x = Integer.MAX_VALUE;
+			if (v == -1) v = Integer.MAX_VALUE;
+			
+			if (v < c && v < x) {
+				output = output.substring(0, v)
+						.concat(clipboard)
+						.concat(output.substring(v + 8));
+				continue;
+			}
+			
+			clipboard = output.substring(0, c < x ? c : x);
+			
+			if (c < x) {
+				output = clipboard.concat(output.substring(c + 8));
+			} else {
+				output = output.substring(x + 8);
+			}
+			
 		}
+		
+		System.out.println(output);
+	}
+	
+	private String readFile(String filename) throws FileNotFoundException {
+		File file = new File(filename);
+		
+		Scanner scanner = new Scanner(file);
+		List<String> lines = new ArrayList<>();
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			if ("[".equals(line) || "]".equals(line))
+				continue;
+			
+			line = line.trim().replace("\"","");
+			
+			lines.add(line);
+		}
+		
+		return lines.stream().collect(Collectors.joining());
 	}
 	
 	public static void main(String[] args) {
-		new Main();
+		try {
+			new Main();
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
